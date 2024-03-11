e2f87e6f570cf5b59793e67e3f4d3d8376ee5d0d
ckwork <no@no.com>
2024-03-11 18:40:24 -0500

    first successful csv ouput
    yaaay

--- splunk_detections_parser.py
+++ splunk_detections_parser.py
@@ -14,11 +14,31 @@ def fetch_page_data(url):
     if response.status_code == 200:
         return BeautifulSoup(response.text, 'html.parser')
     else:
+        print(response.status_code)
         print(f"Error fetching {url}")
         return None
 
 # parses page for specific information and makes a dictionary output.
-def parse_page(soup, link):
+
+
+# takes a dictionary and outputs a csv with headers respective to the order of the dictionary
+def save_to_csv(data, filename="splunk_research_output.csv"):
+    print(data)
+    keys = data.keys()
+    file_exists = os.path.isfile(filename)
+    with open(filename, 'a', newline='', encoding='utf-8') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+
+        if not file_exists:  # Only write the header if the file doesn't exist
+            dict_writer.writeheader()
+
+        dict_writer.writerow(data)
+    print(f"{data.get('title')} retrieved and written")
+
+# reads the specified url, parses, and then sends it to a csv
+def parse_endpoint_url(url):
+    soup = fetch_page_data(url)
+
     # Assuming the structure based on common patterns; you may need to adjust these
 
     # macros parse
@@ -30,7 +50,7 @@ def parse_page(soup, link):
 
     # parsed data dictionary
     data = {
-        "url": link if link else "N/A",
+        "url": url if url else "N/A",
         "title": soup.find("h1", id="page-title").text.strip() if soup.find("h1", id="page-title") else "N/A",
         "mitre_attack_codes": [tcode.text.strip() for tcode in soup.find('h4', id='attck').find_next_sibling('table').find_all('a')] if soup.find('h4', id='attck').find_next_sibling('table').find_all('a') else "N/A",
         "description": soup.find("h4", id="description").find_next_sibling("p").text.strip() if soup.find("h4", id="description").find_next_sibling("p").text.strip() else "N/A",
@@ -40,36 +60,12 @@ def parse_page(soup, link):
         "required_macros": list(filter(lambda item: item != '', my_macros)),
         "required_fields": list(filter(lambda item: item != '', h4_req_fields)),
         "false_positives": soup.find('h4', id='known-false-positives').find_next_sibling("p").text.strip() if soup.find('h4', id='known-false-positives').find_next_sibling("p").text.strip() else "N/A",
-        "associated_analytics": [code.text.strip() for code in soup.findAll("a", href=True) if "stories" in code['href'] if
-                       "Analytic Stories" not in code],
-        "analytic_stories": [urljoin("https://research.splunk.com", code.get("href")) for code in
-                        soup.findAll("a", href=True) if "stories" in code['href'] if "Analytic Stories" not in code],
-        "references": [href.get("href") for href in soup.find('h4', id='reference').find_next_sibling('ul').findAll("a", href=True)],
+        "associated_analytics": [code.text.strip() for code in soup.findAll("a", href=True) if "stories" in code['href'] if "Analytic Stories" not in code],
+        "analytic_stories": [urljoin("https://research.splunk.com", code.get("href")) for code in soup.findAll("a", href=True) if "stories" in code['href'] if "Analytic Stories" not in code],
+        "references": [href.get("href") for href in soup.find('h4', id='reference').find_next_sibling('ul').findAll("a", href=True)] if soup.find('h4', id='reference').find_next_sibling('ul') else "N/A",
         "tags": [tag.text.strip() for tag in soup.find("span", itemprop="keywords").find_all("a") if "tags" in tag["href"]],
         "category": [cat.text.strip() for cat in soup.find_all("a", href=True) if "categories" in cat["href"]]
     }
-    return data
-
-# takes a dictionary and outputs a csv with headers respective to the order of the dictionary
-def save_to_csv(data, filename="splunk_research_output.csv"):
-
-    keys = data[0].keys()
-    file_exists = os.path.isfile(filename)
-    with open(filename, 'a', newline='', encoding='utf-8') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-
-        if not file_exists:  # Only write the header if the file doesn't exist
-            dict_writer.writeheader()
-
-        dict_writer.writerows(data)
-    print(f"{data.get('title')} retrieved and written")
-
-# reads the specified url, parses, and then sends it to a csv
-def parse_endpoint_url(url):
-    print(url)
-    soup = fetch_page_data(url)
-    print(soup)
-    data = parse_page(soup, url)
 
     save_to_csv(data)
 
@@ -109,8 +105,10 @@ def verify_detections_repo(url):
 def parse_all_repo_links():
     with open('./detections_repo_links.txt', 'r') as file:
         for line in file:
-            print(line, type(line))
-            parse_endpoint_url(line)
+            print(line)
+            if fetch_page_data(line.strip('\n')) is None:
+                pass
+            parse_endpoint_url(line.strip('\n'))
 
 
 if __name__ == "__main__":
